336741f2e7191938263e50ba81790da0557f6f6c
Alex Wayne <alex@beautifulpixel.com>
2026-02-16 17:00:02 -0800

    Improved example

--- examples/index.ts
+++ examples/index.ts
@@ -1,25 +1,6 @@
-import { createShaderCanvas } from 'typegpu-shader-canvas'
-import { f32, vec2f, vec3f, vec4f } from 'typegpu/data'
-import { abs, length, select, sin, smoothstep } from 'typegpu/std'
+import { highlight } from 'sugar-high'
 
-createShaderCanvas(
-  document.getElementById('canvas'),
-  ({ uv, time, mouse, aspectRatio }) => {
-    'use gpu'
+import './example'
+import source from './example.ts?raw'
 
-    let color = vec3f()
-
-    const r = smoothstep(0, 0.08, abs(uv.y + sin(time * 3 + uv.x * 4) * 0.2))
-    const g = smoothstep(0, 0.07, abs(uv.y + sin(time * 2 + uv.x * 5) * 0.2))
-    const b = smoothstep(0, 0.06, abs(uv.y + sin(time * 1 + uv.x * 6) * 0.2))
-    color = color.add(vec3f(1).sub(vec3f(r, g, b)))
-
-    const mouseDistance = mouse.uv.sub(uv)
-    color = color.add(
-      smoothstep(0.2, 0, length(mouseDistance.div(vec2f(1, aspectRatio)))) *
-        select(0.5, f32(1), mouse.down === 1),
-    )
-
-    return vec4f(color, 1)
-  },
-).startRendering()
+document.getElementById('code')!.innerHTML = highlight(source)
